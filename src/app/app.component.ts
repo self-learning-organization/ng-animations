ba87a07a4cb514bdcc91010b6234bf712d7d2fda
Chantal Neo <Chantal@hotmail.sg>
2020-03-22 00:14:35 +0800

    Using Animation Callbacks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -125,4 +125,12 @@ export class AppComponent {
     onShrink() {
       this.wildState = 'shrunken';
     }
+
+    animationStarted(event) {
+      console.log(event)
+    }
+    
+    animationEnded(event) {
+      console.log(event)
+    }
 }
